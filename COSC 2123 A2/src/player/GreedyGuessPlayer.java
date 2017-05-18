a2dbd714de39d579fc74d87a088c0b3f8c9968eb
Ka Kyen Lam <lamkk91@gmail.com>
2017-05-18 18:09:15 +1000

    Modified the general methods (all except the guess)

--- COSC 2123 A2/src/player/GreedyGuessPlayer.java
+++ COSC 2123 A2/src/player/GreedyGuessPlayer.java
@@ -2,6 +2,8 @@ package player;
 
 import java.util.Random;
 import java.util.Scanner;
+
+import ship.Ship;
 import world.World;
 import world.World.Coordinate;
 import world.World.ShipLocation;
@@ -13,33 +15,71 @@ import world.World.ShipLocation;
  */
 public class GreedyGuessPlayer implements Player {
 
-	private World world;
-	private Answer answer;
-	private Guess guess = new Guess();
+	public class OwnShip {
+		Ship ship = null;
+		int[] rowCoord = { -1, -1, -1, -1, -1 };
+		int[] colCoord = { -1, -1, -1, -1, -1 };
+		boolean[] isdown = { true, true, true, true, true };
+
+		private OwnShip() {
+		}
+	}
+
+	private int rowSize = 0;
+	private int colSize = 0;
+	boolean[][] isGuessed;
+	OwnShip[] ownShips = new OwnShip[5];
+
 	private int rowGuess = 0;
 	private int colGuess = 0;
 	private int rowHunt = 0;
 	private int colHunt = 0;
 	private boolean isHunt = false;
-	private int backDist = 3;
+	private int backDist = 2;
 	private int pos = 0;
 
 	@Override
 	public void initialisePlayer(World world) {
-		this.world = world;
-		this.answer = new Answer();
+		this.rowSize = world.numRow;
+		this.colSize = world.numColumn;
+		this.isGuessed = new boolean[this.rowSize][this.colSize];
+		int i = 0;
+		for (World.ShipLocation shiploc : world.shipLocations) {
+			this.ownShips[i] = new OwnShip();
+			this.ownShips[i].ship = shiploc.ship;
+			for (int j = 0; j < this.ownShips[i].ship.len(); j++) {
+				this.ownShips[i].rowCoord[j] = shiploc.coordinates.get(j).row;
+				this.ownShips[i].colCoord[j] = shiploc.coordinates.get(j).column;
+				this.ownShips[i].isdown[j] = false;
+			}
+			i++;
+		}
+		
+
 	} // end of initialisePlayer()
 
 	@Override
 	public Answer getAnswer(Guess guess) {
-		answer.isHit = false;
-		for (ShipLocation shiploc : world.shipLocations) {
-			for (Coordinate pos : shiploc.coordinates)
-				if (guess.row == pos.row && guess.column == pos.column) {
+		Answer answer = new Answer();
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < this.ownShips[i].ship.len(); j++) {
+				if ((guess.row == this.ownShips[i].rowCoord[j]) && guess.column == this.ownShips[i].colCoord[j]) {
 					answer.isHit = true;
-					isHunt = true;
+					this.ownShips[i].isdown[j] = true;
+					boolean isSunk = true;
+					for (int k = 0; k < this.ownShips[i].ship.len(); k++) {
+						if (this.ownShips[i].isdown[k] == false) {
+							isSunk = false;
+						}
+					}
+
+					if (isSunk) {
+						answer.shipSunk = this.ownShips[i].ship;
+					}
 					return answer;
 				}
+			}
+
 		}
 
 		return answer;
@@ -56,7 +96,7 @@ public class GreedyGuessPlayer implements Player {
 		}
 	}
 
-	public void huntInit(int pos, Answer answer) {
+	public void huntInit(int pos) {
 
 		// Initial
 		// colGuess + 1;
@@ -89,7 +129,7 @@ public class GreedyGuessPlayer implements Player {
 	// if failed
 	// row/col - backtrack
 
-	public void huntProgress(int pos, Answer answer) {
+	public void huntProgress(int pos) {
 
 		boolean isbackTrack = false;
 
@@ -118,10 +158,10 @@ public class GreedyGuessPlayer implements Player {
 				rowHunt += backDist;
 			}
 			isbackTrack = true;
-			backDist = 3;
+			backDist = 2;
 		}
 
-		if (answer.isHit && isbackTrack == true) {
+		if (isbackTrack == true) {
 			if (pos == 0) {
 				colHunt--;
 			} else if (pos == 1) {
@@ -132,39 +172,52 @@ public class GreedyGuessPlayer implements Player {
 				rowHunt++;
 			}
 		}
-		
-		
 
 	}
 
 	@Override
 	public Guess makeGuess() {
-		if (isHunt) {
-			huntInit(pos , answer);
-			//huntProgress(pos, answer);
-			guess.row = rowHunt;
-			guess.column = colHunt;
-			if (pos < 4) {
-				pos++;
-			}
-		} else {
-			guess.row = rowGuess;
-			guess.column = colGuess;
-			generateNextGuess();
-		}
-		// dummy return
-		return guess;
+		Guess guess = new Guess();
+		
+		do {
+		if (isHunt && this.isGuessed[guess.row][guess.column]) {
+		 huntInit(pos);
+		 //huntProgress(pos);
+		 guess.row = rowHunt;
+		 guess.column = colHunt;
+		 if (pos < 4) {
+		 pos++;
+		 }
+		 } else {
+		 guess.row = rowGuess;
+		 guess.column = colGuess;
+		 generateNextGuess();
+		 }
+		 }while (this.isGuessed[guess.row][guess.column]);
+		
+		this.isGuessed[guess.row][guess.column] = true;
+			
+		 // dummy return
+		 return guess;
 	} // end of makeGuess()
 
 	@Override
 	public void update(Guess guess, Answer answer) {
-
+		if (answer.isHit == true) {
+			isHunt = true;
+		}
 	} // end of update()
 
 	@Override
 	public boolean noRemainingShips() {
-
-		return false;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < this.ownShips[i].ship.len(); j++) {
+				if (this.ownShips[i].isdown[j] == false) {
+					return false;
+				}
+			}
+		}
+		return true;
 	} // end of noRemainingShips()
 
 } // end of class GreedyGuessPlayer
