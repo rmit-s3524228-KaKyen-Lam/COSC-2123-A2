7ec7fe03a6a8f465a60e7fadfdc68514966e45a8
jhoxton <jakehnovak@gmail.com>
2017-05-26 22:25:37 +1000

    Update RandomGuessPlayer.java

--- COSC 2123 A2/src/player/RandomGuessPlayer.java
+++ COSC 2123 A2/src/player/RandomGuessPlayer.java
@@ -17,7 +17,7 @@ public class RandomGuessPlayer implements Player{
 		Ship ship = null;
 		int[] rowCoord = { -1, -1, -1, -1, -1 };
 		int[] colCoord = { -1, -1, -1, -1, -1 };
-		boolean[] isdown = { true, true, true, true, true }; //Checks status of each ship
+		boolean[] isdown = { true, true, true, true, true }; //Checks status of players ships
 		
 		private RandomShip() {
 		}
@@ -38,7 +38,7 @@ public class RandomGuessPlayer implements Player{
 		this.isGuessed = new boolean[this.rowSize][this.colSize];
 		
 		int i = 0;
-		//Sets random ship locations to the world
+		
 		for (World.ShipLocation shiploc : world.shipLocations) {
 			this.playerShips[i] = new RandomShip();
 			this.playerShips[i].ship = shiploc.ship;
@@ -54,15 +54,19 @@ public class RandomGuessPlayer implements Player{
 
 	} // end of initialisePlayer()
 
+	/**
+	* Loops over players ship checking guess parameter against this players ship locations
+	*/
     @Override
     public Answer getAnswer(Guess guess) {
-	//Loops over players ship checking guess parameter against this players ship locations
+	
     	Answer answer = new Answer();
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 5; i++) {//Loops to number of ships
 			
-			for (int j = 0; j < this.playerShips[i].ship.len(); j++) {
+			for (int j = 0; j < this.playerShips[i].ship.len(); j++) {//Loops over each ship length
 			
 				if ((guess.row == this.playerShips[i].rowCoord[j]) && guess.column == this.playerShips[i].colCoord[j]) {
+					//Checks ships coordinates with guess to find a hit
 					answer.isHit = true;
 					this.playerShips[i].isdown[j] = true;
 					boolean isSunk = true;
@@ -82,9 +86,12 @@ public class RandomGuessPlayer implements Player{
 		}
 
 		return answer;
-	} 
+	}
+    /**
+    * Creates two random ints within the world size to return as shot coordinates 
+    */
     @Override
-    public Guess makeGuess() //Creates two random ints within the world size to return as shot coordinates 
+    public Guess makeGuess() 
         Random rando = new Random();
         int i;
         int j;
@@ -92,6 +99,7 @@ public class RandomGuessPlayer implements Player{
 	        	{
 	          i = rando.nextInt(this.rowSize);
 	          j = rando.nextInt(this.colSize);
+			//Set col and row within the world space limits
 	       
 	        	} while (this.isGuessed[i][j] != false);
 	        
